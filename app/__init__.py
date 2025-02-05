152712b12689c4f6e9e7745ea97ca51425f9af9e
Daniel Salin <daniel.salin@gmail.com>
2025-02-05 12:52:13 +0100

    Db init and Register routes to app

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,24 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from app.routes import bp
 
 config = Config()
+db = SQLAlchemy()
+login_manager=LoginManager()
+login_manager.login_view = "login"
 
 def create_app():
     app = Flask(__name__)
     # Store our configurations in the Flask app
     app.config.from_object(config)
+    db.init_app(app)
+    #Registers all the routes in the app's blueprint
+    app.register_blueprint(bp)
+
+    with app.app_context():
+        from . import routes, models
+        db.create_all()
+
     return app
