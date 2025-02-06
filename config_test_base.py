9fc3552a287a83266c938439bd13f0cedfc655f1
Daniel Salin <daniel.salin@gmail.com>
2025-02-06 19:47:47 +0100

    Update gitignore, config redirect url and main (formatting)

--- config_test_base.py
+++ config_test_base.py
@@ -1,6 +1,6 @@
 class Config:
     CLIENT_ID = ""
     CLIENT_SECRET = ""
-    REDIRECT_URI = ""
+    REDIRECT_URI = "http://localhost:5000/callback"
     SECRET_KEY = ""
     SQLALCHEMY_DATABASE_URI = "sqlite:///testing.db"
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from app import create_app
 from config import Config
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     create_app(Config).run(debug=True)
-
