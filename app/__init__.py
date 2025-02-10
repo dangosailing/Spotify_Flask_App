2b5420a6dcc38980b4b7629d4789d957a3e05657
Daniel Salin <daniel.salin@gmail.com>
2025-02-10 12:15:06 +0100

    Create instance of csrf in app

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,22 @@
 from flask import Flask
 from app.routes import bp
 from config import Config
-from app.extensions import db, bp, login_manager
+from app.extensions import db, bp, login_manager, csrf
 
 
 def create_app(config: Config) -> Flask:
     """Create an instance of the Spotify App"""
     app = Flask(__name__)
+
     # Store our configurations in the Flask app
     app.config.from_object(config)
     # Registers all the routes in the app's blueprint
     # Login
     login_manager.init_app(app)
+    csrf.init_app(app)
     with app.app_context():
-        from . import models 
+        from . import models
+
         app.register_blueprint(bp)
         db.init_app(app)
         db.create_all()
