d54aa6e053faa3797f6eeb4c9161c61cba8fe494
Daniel Salin <daniel.salin@gmail.com>
2025-02-10 16:39:55 +0100

    Update api_geo docstrings

--- app/api_geo.py
+++ app/api_geo.py
@@ -3,12 +3,13 @@ import requests
 import json
 
 def get_lat_long(ip: str = "me") -> list:
-    """Returns the Latitude and Longitude in list format, defaults to"""
+    """Returns the Latitude and Longitude in list format, defaults to local ip"""
     g = geocoder.ip(ip)
     return g.latlng
 
 
-def get_weather(lat, long):
+def get_weather(lat, long) -> dict:
+    """ Fetch weather data from open-meteo"""
     url = "https://api.open-meteo.com/v1/forecast"
     params = {
         "latitude": lat,
@@ -19,13 +20,15 @@ def get_weather(lat, long):
     return requests.get(url=url, params=params).json()
 
 
-def convert_weather_code(weather_code: str):
+def convert_weather_code(weather_code: str) -> str:
+    """ Reads the weather code json file and returns the matching value """
     with open("app/weather_codes.json") as weather_codes:
         codes_dict = json.load(weather_codes)
         return codes_dict[weather_code]
 
 
-def get_weather_conditions():
+def get_weather_conditions() -> str:
+    """ Returns current weather as a string value """
     lat_long = get_lat_long()
     weather_data = get_weather(lat_long[0], lat_long[1])
     weather_code = weather_data["current_weather"]["weathercode"]
