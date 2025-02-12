86fbafb27c0cdbeaedf1d1758dc1551b3f991106
daniel-salin <daniel.salin@gmail.com>
2025-02-12 14:48:11 +0100

    rename test_functions to test_auth_handler

--- tests/test_auth_handler.py
+++ tests/test_auth_handler.py
@@ -1,17 +1,16 @@
 from app.auth_handler import AuthHandler
 
-def test_create_user():
+def test_create_user_with_fixture(test_user):
     """
-    GIVEN a create user function
+    GIVEN a create user function with the test_user fixture
     WHEN the create user function is called with a username and password
     THEN make sure the returned user has the same username and make sure that password is not stored in clear text
     """
-    
     username= "John Doe"
-    password= "test123"
+    password= "password123"
     auth_handler = AuthHandler()
     user = auth_handler.create_user(username=username, password=password)
     
-    assert user.username == "John Doe"
+    assert user.username == test_user.username
     # Ensure password is hashed
-    assert user.password != "test123"
+    assert user.password != test_user.password
