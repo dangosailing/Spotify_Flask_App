27ceae9bf78e37b40c691fca02c1955b13512c98
daniel-salin <daniel.salin@gmail.com>
2025-02-12 14:45:21 +0100

    Remove test_app fixture from conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,5 @@
 import pytest
 from app.models import User
-from app import create_app
-from tests.config_test import Config
 
 # SCOPES allow us to decide when the fixture is run
 # function - once per test class
@@ -15,12 +13,3 @@ def test_user():
     
     user = User(username=username, password=password)
     return user
-
-# Scope set to session to ensure all routes can be tested in the same run
-@pytest.fixture(scope="session")
-def test_app():
-    app = create_app(Config)
-    # Creating a testing client allows us to create an instance of the app with our testing config 
-    with app.test_client() as testing_client:
-        yield testing_client
-    
