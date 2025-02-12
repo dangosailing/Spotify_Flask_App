50c1c55ce4c9f06bdb4807bc1b26c4364e4b1e1e
daniel-salin <daniel.salin@gmail.com>
2025-02-12 14:45:38 +0100

    Remove config_test_base

--- config_test_base.py
+++
@@ -1,6 +0,0 @@
-class Config:
-    CLIENT_ID = ""
-    CLIENT_SECRET = ""
-    REDIRECT_URI = "http://localhost:5000/callback"
-    SECRET_KEY = ""
-    SQLALCHEMY_DATABASE_URI = "sqlite:///testing.db"
