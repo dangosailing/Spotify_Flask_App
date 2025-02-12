2f8cbde82289b30b537e4d983d10d7b9c7230a3b
Daniel Salin <daniel.salin@gmail.com>
2025-02-12 17:00:49 +0100

    Add typing for validate_pwd

--- app/utils.py
+++ app/utils.py
@@ -1,6 +1,6 @@
 import re
 
-def validate_pwd(password):
+def validate_pwd(password:str ) -> bool:
     """ Returns true if password matches complexity requirements"""
     # 6 letters minimum, at least one uppercase letter, and one number
     if re.findall(r"^(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{6,}$", password):
